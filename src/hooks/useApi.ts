78cfac29f2773c9a188edc2bb0efec121b4b2e3b
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-26 22:48:27 +0300

    Add src/hooks/useApi.ts

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+function useApi(url: string) {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<Error | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        const data = await response.json()
+        setData(data)
+      } catch (error) {
+        setError(error as Error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [url])
+
+  return { data, loading, error }
+}
+
+export default useApi
